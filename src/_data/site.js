9d377bc93376aedbdbcbfb14005afadb846a8bc1
SaimAbbasi <saim@users.noreply.github.com>
2026-04-11 02:49:12 +0400

    feat: add site data and base layout

---
+++ src/_data/site.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "Vullnet Nura Group",
+  shortName: "VNG",
+  baseUrl: "https://vullnet.vercel.app",
+  defaultDescription: "Ontario's deadline-first commercial interior execution contractor. 1000+ projects delivered on time, on budget.",
+  email: "info@vullnetnuragroup.com"
+};
